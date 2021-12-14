01981ba343cfa4d6bec2116d265ca9474c592381
Marchelo1989 <marchelo.1989@live.cl>
2021-12-14 01:44:32 -0300

    v1.18 Carga de los Host de 9.00
    implemntacion de la pagina para los Firmware 9.00 y otros ajustes

--- PS4/js/detectar.js
+++ PS4/js/detectar.js
@@ -1,6 +1,6 @@
 //########################## End ROP Framework functions by bguerville(under development) #########################
 function ps4chk(){
-	var fwCompat = ["5.05", "6.72", "7.02", "7.50", "7.51", "7.55"];
+	var fwCompat = ["5.05", "6.72", "7.02", "7.50", "7.51", "7.55", "9.00"];
 	var ua = navigator.userAgent;
 	var uaStringCheck = ua.substring(ua.indexOf("5.0 (") + 5, ua.indexOf(") Apple") - 7);
 	var fwVersion = ua.substring(ua.indexOf("5.0 (") + 19, ua.indexOf(") Apple"));
@@ -9,47 +9,42 @@ function ps4chk(){
 		case "PlayStation":
 			switch (fwVersion) {
 				case fwCompat[0]:
-					alert(msgCongrats);
 					window.location.href ="./5.05/index.html"
-//					initDEX();
 					//5.05();
 					break;
 					
 				case fwCompat[1]:
-					alert(msgCongrats);
 					window.location.href ="./6.72/index.html"
-//					initDEX();
 					//6.72();
 					break;
 					
 				case fwCompat[2]:
 					window.location.href ="./7.02/index.html"
+					//7.02();
 					break;
 					
 				case fwCompat[3]:
-					alert(msgCongrats);
-					//document.write( "Informacion Navegador:"+uaStringCheck+" <p />" );
-					window.location.href ="./PS4.html"
-//					initDEX();
+					window.location.href ="./7.5x/index.html"
 					//7.50();
 					break;
 					
 				case fwCompat[4]:
-					alert(msgCongrats);
-					window.location.href ="./PS4.html"
-//					initDEX();
+					window.location.href ="./7.5x/index.html"
 					//7.51();
 					break;
 					
 				case fwCompat[5]:
-					alert(msgCongrats);
-					window.location.href ="./PS4.html"
-//					initDEX();
+					window.location.href ="./7.5x/index.html"
 					//7.55();
 					break;
 					
+				case fwCompat[6]:
+					window.location.href ="./9.00/index.html"
+					//9.00();
+					break;
+					
 				default:
-					alert('¡Tu PS4 no está en FW 5.05 - 6.72 - 7.02 - 7.50 - 7.51 - 7.55! Su versión actual de FW en ejecución es '+ fwVersion +', que no es compatible con PS4xploit. Todas las funciones han sido deshabilitadas. Tratar de no Actualizar su consola para poder desbloquearla mas adelante a nuevos xploit');
+					alert('¡Tu PS4 no está en FW 5.05 - 6.72 - 7.02 - 7.50 - 7.51 - 7.55 - 9.00! Su versión actual de FW en ejecución es '+ fwVersion +', que no es compatible con PS4xploit. Todas las funciones han sido deshabilitadas. Tratar de no Actualizar su consola para poder desbloquearla mas adelante a nuevos xploit');
 					//document.write( "Informacion Navegador:"+uaStringCheck+" <p />" );
 					disable_PS4();
 					break;
