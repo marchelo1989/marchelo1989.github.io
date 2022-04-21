c4177d7aa74709dd77a05fdc6b3b602488db8d6b
Marchelo1989 <marchelo.1989@live.cl>
2022-04-21 15:47:52 -0400

    v1.54 Cambios menores menu
    Encabezado en auto v3

--- Home/js/menu.js
+++ Home/js/menu.js
@@ -1,5 +1,14 @@
-function sistemaOperativo(){
-    document.getElementById("menu1").innerHTML = '<ul><li><a href="index.html">Página principal</a></li> <li> <span class="opener">Ayuda</span> <ul> <li><a href="ActivarWindows.html">Activación de Windowso Office</a></li> </ul> </li> </ul>';
+function cargarEncabezado(){
+    var barra = 
+        '<li><a href="https://twitter.com/marchelo_bm" class="icon brands fa-twitter" target="_blank"><span class="label">Twitter</span></a></li>'+
+        '<li><a href="https://www.facebook.com/marcelo.bm" class="icon brands fa-facebook-f" target="_blank"><span class="label">Facebook</span></a></li>'+
+        '<li><a href="https://www.instagram.com/marchelo.bm" class="icon brands fa-instagram" target="_blank"><span class="label">Instagram</span></a></li>'+
+        '<li><a href="https://wa.me/message/ZWPJK3PZT3QQJ1" class="icon brands fa-whatsapp" target="_blank"><span class="label">Whatsapp</span></a></li>'+
+        '<li><a href="https://m.me/marcelo.bm" class="icon brands fa-facebook-messenger" target="_blank"><span class="label">Messenger</span></a></li>'+
+        '<li><a href="https://github.com/marchelo1989" class="icon brands fa-github" target="_blank"><span class="label">Github</span></a></li>'+
+        '<li><a href="https://t.me/MarcheloBM" class="icon brands fa-telegram" target="_blank"><span class="label">Telegram</span></a></li>';
+    var el = document.getElementById("encabezado");
+    el.innerHTML = barra;
   
 }
-window.onload = sistemaOperativo;
+window.onload = cargarEncabezado;
