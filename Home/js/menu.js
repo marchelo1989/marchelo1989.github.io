5d46799b26bc9c51b86152e03c2fd93d2fea5bce
Marchelo1989 <marchelo.1989@live.cl>
2022-04-21 14:55:31 -0400

    v1.51 Cambios menores menu
    v3 prueba al menu auto

---
+++ Home/js/menu.js
@@ -0,0 +1,5 @@
+function sistemaOperativo(){
+    document.getElementById("menu1").innerHTML = '<ul><li><a href="index.html">Página principal</a></li> <li> <span class="opener">Ayuda</span> <ul> <li><a href="ActivarWindows.html">Activación de Windowso Office</a></li> </ul> </li> </ul>';
+  
+}
+window.onload = sistemaOperativo;
