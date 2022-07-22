05709329d07af42a113164b388447c30d3e8e746
Marchelo <marchelo.1989@live.cl>
2022-07-22 05:07:57 -0400

    v1.83 Update PS4 Web Host
    Todos los Payloads en Prueba

--- PS4/9.00/js/Info.js
+++ PS4/9.00/js/Info.js
@@ -25,75 +25,9 @@ function infoGoldhen23b2(){
 }
 
 /* Payloads */
-function infoAppDumper(){
-	document.getElementById("msj").innerHTML = "Copiar el juego solo a la unidad USB";
+function allPayloads(){
+	document.getElementById("msj").innerHTML = "Todos los Payloads para 9.00";
 }
-function infoWebActivador(){
-	document.getElementById("msj").innerHTML = "Active su cuenta de usuario desde el navegador de la consola";
-}
-
-
-
-
-/* Payloads */
-function cargarFTP(){
-	document.getElementById("msj").innerHTML = "Accede a esta consola a través de FTP (ten cuidado con lo que haces)";
-}
-
-function cargarModeleDumper(){
-	document.getElementById("msj").innerHTML = "Esta carga útil nos permite volcar los módulos descifrados de /system, /system_ex, /update y la raíz del sistema de archivos en un dispositivo USB";
-}
-function cargarApp2USB(){
-	document.getElementById("msj").innerHTML = "Transferir juegos / aplicaciones a una unidad USB en exFAT";
-}
-function cargarDisableUpdate(){
-	document.getElementById("msj").innerHTML = "Desactivar actualizaciones del sistema";
-}
-function cargarEnableUpdate(){
-	document.getElementById("msj").innerHTML = "Habilitar actualizaciones del sistema";
-}
-function cargarHistoryBlocker(){
-	document.getElementById("msj").innerHTML = "Desactive el historial del navegador, solo mostrará las páginas de uso frecuente una vez que abra";
-}
-
-function cargarLinux1GB(){
-	document.getElementById("msj").innerHTML = "Cargue Linux desde una unidad USB con 1 GB de VRAM";
-}
-function cargarLinux4GB(){
-	document.getElementById("msj").innerHTML = "Cargue Linux desde una unidad USB con 4 GB de VRAM";
-}
-function cargarNetcat(){
-	document.getElementById("msj").innerHTML = "Netcat para firmware 7.5x";
-}
-function cargarBackup(){
-	document.getElementById("msj").innerHTML = "Crea una copia de seguridad de la base de datos de la consola y su usuario en una unidad USB";
-}
-function cargarRestore(){
-	document.getElementById("msj").innerHTML = "Restaurar la copia de seguridad de la base de datos desde la unidad USB";
-}
-function cargarToDex(){
-	document.getElementById("msj").innerHTML = "Habilite algunas funciones de depuración disponibles en TestKit";
-}
-function cargarPs4Debug(){
-	document.getElementById("msj").innerHTML = "Active su cuenta de usuario sin conexión";
-}
-function cargarRIFRenamer(){
-	document.getElementById("msj").innerHTML = "Cambia el tipo de licencia de los juegos instalados de FALSO a GRATIS, úselo para resolver juegos que no funcionarán con la carga útil normal de HEN";
-}
-
-function cargarWebRTE(){
-	document.getElementById("msj").innerHTML = "Requerido para activar trucos a través del sitio web de PS4 Trainer";
-}
-function cargarOrbisToolbox(){
-	document.getElementById("msj").innerHTML = "OrbisToolbox para firmware 7.5x";
-}
-function cargarDisableASLR(){
-	document.getElementById("msj").innerHTML = "DisableASLR para firmware 7.5x";
-}
-function cargarExitlDU(){
-	document.getElementById("msj").innerHTML = "ExitlDU para firmware 7.5x";
-}
-
 function titulopag(){
 	document.getElementById("msj").innerHTML = "PS4 9.00 Host x MarcheBM";
 }
